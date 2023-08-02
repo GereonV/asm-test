7ab6480cfac24a4d0e0b3f4bfba82b22cecaf168
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-08-02 16:42:38 +0200

    Enable smartalign

--- src/clamp.s
+++ src/clamp.s
@@ -1,15 +1,9 @@
 global simple_clamp
 global    opt_clamp
 
+%use smartalign
+alignmode p6
 default rel
-; %define nop2	db	66h, 90h,
-; %define nop3	db	0Fh, 1Fh, 00h,
-; %define nop4	db	0Fh, 1Fh, 40h, 00h,
-; %define nop5	db	0Fh, 1Fh, 44h, 00h, 00h,
-; %define nop6	db	66h, 0Fh, 1Fh, 44h, 00h, 00h,
-; %define nop7	db	0Fh, 1Fh, 80h, 00h, 00h, 00h, 00h,
-; %define nop8	db	0Fh, 1Fh, 84h, 00h, 00h, 00h, 00h, 00h,
-; %define nop9	db	66h, 0Fh, 1Fh, 84h, 00h, 00h, 00h, 00h, 00h,
 
 section .text
 
