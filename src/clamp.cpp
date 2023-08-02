0080437707360b867456bde25ff3503d2ac40527
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-08-03 01:29:58 +0200

    Improve testing

--- src/clamp.cpp
+++ src/clamp.cpp
@@ -4,12 +4,13 @@
 #include <algorithm>
 #include <chrono>
 #include <execution>
+#include <iomanip>
 #include <iostream>
 #include <memory>
 
 using clamp_func = void(std::uint32_t * data, std::uint64_t count) noexcept;
 
-extern "C" clamp_func simple_clamp, opt_clamp; // handwritten assembly
+extern "C" clamp_func simple_clamp, cmov_clamp, opt_clamp; // handwritten assembly
 
 extern "C" [[gnu::noinline]] void replace_if(std::uint32_t * data, std::uint64_t count) noexcept {
 	std::replace_if(std::execution::par_unseq, data, data + count, [](auto x) { return x > 255; }, 255);
@@ -28,12 +29,13 @@ struct func_t {
 
 inline constexpr func_t functions[]{
 	FUNC(simple_clamp),
+	FUNC(cmov_clamp),
 	FUNC(replace_if),
 	FUNC(transform),
-	// FUNC(opt_clamp),
+	FUNC(opt_clamp),
 };
 
-int main(int, char ** argv) {
+int main() {
 	using namespace std::chrono;
 	using clock = high_resolution_clock;
 	constexpr auto max_count = 1 << 20;
@@ -43,10 +45,10 @@ int main(int, char ** argv) {
 	for(auto && f : functions) {
 		#if 1
 		std::cout << "Profiling function: " << f.name << '\n';
-		for(std::uint64_t count{max_count >> 5}; count <= max_count; count <<= 1) {
+		for(std::uint64_t count{max_count >> 9}; count <= max_count; count <<= 1) {
 			nanoseconds duration_ns{0};
 			std::uint64_t iterations = 0;
-			while(duration_ns < seconds{2}) {
+			while(duration_ns < milliseconds{1500}) {
 				std::memcpy(buf.get(), arr.get(), 4 * count);
 				auto start = clock::now();
 				f.func(buf.get(), count);
@@ -57,16 +59,17 @@ int main(int, char ** argv) {
 			duration<double> duration_s{duration_ns};
 			auto seconds = duration_s.count();
 			std::cout <<
-				"count=" << std::setw(6) << count <<
+				"count=" << std::setw(7) << count <<
 				"\tclamps=" << std::setw(10) << iterations * count <<
 				"\tclamps/s=" << iterations * count / seconds << std::endl;
 		}
 		std::cout << '\n';
 		#else
-		std::transform(std::execution::par_unseq, arr.get(), arr.get() + max_count, arr.get(), [](auto x) { return x % 512; });
 		std::cout << "Testing function: " << f.name << std::flush;
 		for(std::uint64_t alignment{4}; alignment <= 64; alignment *= 2) {
 			auto offset = (64 + alignment - ((std::uintptr_t) buf.get() % 64)) / 4;
+			arr[offset] = 10;
+			arr[offset + 1] = -10;
 			for(std::uint64_t count{}; count <= 512; ++count) {
 				auto ptr = buf.get() + offset;
 				auto og  = arr.get() + offset;
@@ -74,6 +77,7 @@ int main(int, char ** argv) {
 				f.func(ptr, count);
 				if(!std::ranges::equal(og, og + count, ptr, ptr + count, {}, [](auto x) { return x > 255 ? 255 : x; })) {
 					std::cout << " - function failed\n";
+					std::cout << "count=" << count << "\talignment=" << alignment << std::endl;
 					return 1;
 				}
 			}
