d94f51b9f5306c40d9bffd638ebb353d423c610e
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-08-02 02:01:18 +0200

    Implement clamping with AVX2

--- src/clamp.s
+++ src/clamp.s
@@ -1,4 +1,5 @@
 global simple_clamp
+global    opt_clamp
 
 default rel
 ; %define nop2	db	66h, 90h,
@@ -27,3 +28,61 @@ simple_clamp:
 	jne	.LOOP
 .RET:
 	ret
+
+	align	16
+opt_clamp:
+	; jump if too small
+	cmp	rsi, 8
+	jb	simple_clamp
+
+	; setup constants
+	mov	ecx, 8
+	mov	edx, 32
+	vmovdqa	ymm1, [and_mask]
+
+	; unaligned first packed clamp
+	vmovdqu	ymm0, [rdi]
+	vpand	ymm0, ymm0, ymm1
+	vmovdqu	[rdi], ymm0
+
+	; align rdi to next 32 bytes
+	; subtract from count in rsi accordingly
+	; rsi -= (32-rdi%32)/4
+	; rdi += 32-(rdi%32)
+	mov	rax, rdi
+	and	rdi, ~31
+	add	rdi, rdx
+	sub	rax, rdi
+	sar	rax, 2
+	add	rsi, rax
+
+	; no aligned moves possible
+	cmp	rsi, rcx
+	jb	.LAST
+
+	align	16
+.LOOP:
+	; aligned packed clamp
+	vmovdqa	ymm0, [rdi]
+	vpand	ymm0, ymm0, ymm1
+	vmovdqa	[rdi], ymm0
+
+	sub	rsi, rcx
+	add	rdi, rdx
+	cmp	rsi, rcx
+	jae	.LOOP
+.LAST:
+	; unaligned packed clamp
+	sub	rsi, rcx
+	vmovdqu	ymm0, [rdi + 4 * rsi]
+	vpand	ymm0, ymm0, ymm1
+	vmovdqu	[rdi + 4 * rsi], ymm0
+	vzeroupper
+	ret
+
+
+section .data
+
+	align	32
+and_mask:
+	dd	255, 255, 255, 255, 255, 255, 255, 255
