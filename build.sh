09bc7b08bf7ed5d5def46b222a555658351d61d0
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-08-03 01:28:59 +0200

    Enable all optimizations

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mkdir -p obj/ bin/
 nasm -felf64 -o obj/clamp.s.o src/clamp.s
-g++ -std=c++20 -c -O3 -o obj/clamp.cpp.o src/clamp.cpp
+g++ -std=c++20 -march=native -c -O3 -o obj/clamp.cpp.o src/clamp.cpp
 g++ -o bin/clamp obj/clamp.*.o
